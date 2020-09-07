045c5286c0070acb6aade1a4f42ffd1fe5bcc87d
xiekun2019 <a418165301@163.com>
2020-09-07 10:41:56 +0800

    插入排序

---
+++ src/com/xiekun/algs4/sort/Insertion.java
@@ -0,0 +1,46 @@
+package com.xiekun.algs4.sort;
+
+import com.xiekun.algs4.util.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Insertion {
+    // 插入排序
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 1; i < N; i++) {
+            for(int j = i; j>0 && less(a[j], a[j-1]); j--){
+                exch(a, j, j-1);
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j){
+        Comparable t = a[i]; a[i] = a[j]; a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        // 在单行中打印数组
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        // 测试数组元素是否有序
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // 从标准输入读取字符串，将它们排序并输出
+        String[] a = new StdIn("E:\\algs4-data\\tiny.txt").readAllStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
